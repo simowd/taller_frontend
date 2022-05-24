734db4552927a99f1779475220b3631f0e6d2711
Martin Campuzano <martin.campuzano.flores@gmail.com>
2022-05-24 15:00:26 -0400

    Added console instance landing page

---
+++ src/components/LandingPageConsole/editorOptions.js
@@ -0,0 +1,17 @@
+const editorOptions = {
+  accessibilitySupport: 'on',
+  autoClosingBrackets: 'never',
+  bracketPairColorization: 'on',
+  folding: false,
+  fontFamily: 'Source Code Pro',
+  fontSize: '16',
+  links: false,
+  minimap: {
+    enabled: false,
+  },
+  lineNumbers: 'off',
+  readOnly: true,
+  tabCompletion: 'off',
+};
+
+export default editorOptions;
---
+++ src/components/LandingPageConsole/index.js
@@ -0,0 +1,56 @@
+import { Flex, Spinner } from '@chakra-ui/react';
+import Editor, { loader } from '@monaco-editor/react';
+import React, { useRef } from 'react';
+import editorOptions from './editorOptions';
+
+const ConsoleInstance = () => {
+  const monacoRef = useRef(null);
+
+  //Set editor language  
+  loader.config({
+    paths: {
+      //vs: '/monaco-editor'
+    },
+    'vs/nls': {
+      availableLanguages: {
+        '*': 'es',
+      }
+    }
+  });
+
+  // eslint-disable-next-line no-unused-vars
+  const handleEditorWillMount = (monaco) => {
+    //Enable Accesability support default
+    //monaco.editor.EditorOptions.accessibilitySupport = 2;
+  };
+
+  // eslint-disable-next-line no-unused-vars
+  const handleEditorDidMount = (editor, monaco) => {
+    monacoRef.current = editor;
+    monaco.editor.remeasureFonts();
+  };
+
+  //Render the editor once the user has been loaded
+  const renderEditor = () => {
+    return (
+      <Editor
+        defaultLanguage='plaintext'
+        value={''}
+        onMount={handleEditorDidMount}
+        beforeMount={handleEditorWillMount}
+        loading={<Spinner size={'lg'} color={'purple.400'}
+        />}
+        theme={'vs-dark'}
+        options={editorOptions}
+      />
+    );
+  };
+
+  return (
+    <Flex h={'98%'} w={'100%'} alignSelf={'center'} justify={'center'}>
+      {renderEditor()}
+    </Flex>
+  );
+};
+
+export default ConsoleInstance;
