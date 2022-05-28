c4321786f782a05526f3f85969c1b96793b58871
Martin Campuzano <martin.campuzano.flores@gmail.com>
2022-05-28 00:12:36 -0400

    Changed sound volume

--- src/hooks/Sound.js
+++ src/hooks/Sound.js
@@ -3,7 +3,7 @@ import useSound from 'use-sound';
 
 const useAccesibleSound = (audioFile) => {
   const settings = useSelector(state => state.settings);
-  const [play] = useSound(audioFile);
+  const [play] = useSound(audioFile, { volume: 0.5 });
 
   const playSound = () => {
     if (settings.audio_feedback) {
